3b3e6ef3d23ad6abfe7072e215c3b0f8ad51bb29
Levin Keller <post@levinkeller.de>
2023-04-20 10:52:40 +0200

    chore: fix issues and add ci

--- next.config.js
+++ next.config.js
@@ -4,12 +4,14 @@
  * @type {import('next').NextConfig}
  **/
 
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
 })
 
-module.exports = withNextra()
+export default withNextra()
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })

26511f3649edb645d69430907e51c86caa35dc46
Levin Keller <post@levinkeller.de>
2023-06-28 09:59:31 +0200

    feat: füge einen Button hinzu, der es ermöglicht, neue Veranstaltungen einzutragen

--- components/Events/EventsPage.tsx
+++ components/Events/EventsPage.tsx
@@ -8,22 +8,25 @@ export const EventOverviewPage: React.FC<{ events: EventProps[] }> = ({
   const groupedEvents = groupEvents(events)
 
   return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-4xl font-bold mb-8">
-        Veranstaltungen in unserer Stadt
-      </h1>
+    <>
+      <div className="container mx-auto p-8">
+        <h1 className="text-4xl font-bold mb-8">
+          Veranstaltungen in unserer Stadt
+        </h1>
 
-      {Object.keys(groupedEvents).map((month) => (
-        <div key={month} className="mb-8">
-          <h2 className="text-2xl font-bold mb-4">{month}</h2>
+        {Object.keys(groupedEvents).map((month) => (
+          <div key={month} className="mb-8">
+            <h2 className="text-2xl font-bold mb-4">{month}</h2>
 
-          {groupedEvents[month].map((event, index) => (
-            //TODO: Remove the ugly id hack!
-            <Event key={index} id="" {...event} />
-          ))}
-        </div>
-      ))}
-    </div>
+            {groupedEvents[month].map((event, index) => (
+              //TODO: Remove the ugly id hack!
+              <Event key={index} id="" {...event} />
+            ))}
+          </div>
+        ))}
+      </div>
+      <StickyButton />
+    </>
   )
 }
 
@@ -36,3 +39,23 @@ const groupEvents: (
   }),
   {} as Record<string, EventProps[]>
 )
+
+const StickyButton = () => {
+  const handleClick = () => {
+    window.open(
+      'https://docs.google.com/forms/d/e/1FAIpQLSd7uyj3bMztiZZMhCc2dZR-Zr3sZBrEqIi7ZJSbFTwjbbS4QQ/viewform?usp=sf_link'
+    )
+  }
+
+  return (
+    <button
+      className="fixed bottom-4 right-4 z-50 p-4 text-white bg-green-700 rounded-full min-w-200 shadow-lg lg:bottom-8 lg:right-8"
+      onClick={handleClick}
+    >
+      <span className="hidden md:inline">Veranstaltung hinzufügen</span>
+      <span className="md:hidden px-2 text-lg">+</span>
+    </button>
+  )
+}
+
+export default StickyButton

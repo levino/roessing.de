fe62ffa77f6cc7e61bd699699ccd2580697e3e94
Levin Keller <post@levinkeller.de>
2023-06-29 17:17:18 +0200

    feat: verbessere die mobile Ansicht der Veranstaltungen

--- components/Events/Event.tsx
+++ components/Events/Event.tsx
@@ -9,20 +9,44 @@ type Props = EventProps & {
 
 export const Event: React.FC<Props> = (event) => (
   <>
-    <Link href={getHref(event)} target={getTarget(event)} className="flex mb-4">
-      <div className="w-1/3 my-auto">
-        <Image
-          src={event.image?.[0] || ''}
-          alt="Event Preview"
-          width={300}
-          height={200}
-          className="rounded"
-        />
+    <Link className="flex" href={getHref(event)} target={getTarget(event)}>
+      <div className="hidden md:flex flex mb-4">
+        <div className="w-1/3 my-auto">
+          <Image
+            src={event.image?.[0] || ''}
+            alt="Event Preview"
+            width={300}
+            height={200}
+            className="rounded"
+          />
+        </div>
+        <div className="w-2/3 pl-4 my-auto">
+          <p className="font-bold">{event.name}</p>
+          <p className="font-gray-800">{event.location.name}</p>
+          <p className="text-gray-600">{timeAndDate(event.startDate)}</p>
+          <div className="text-sm mt-2 whitespace-pre-line">
+            {event.description}
+          </div>
+        </div>
       </div>
-      <div className="w-2/3 pl-4 my-auto">
-        <p className="font-bold">{event.name}</p>
-        <p className="font-gray-800">{event.location.name}</p>
-        <p className="text-gray-600">{timeAndDate(event.startDate)}</p>
+      <div className="flex flex-col md:hidden mb-8">
+        <div className="flex mb-2">
+          <div className="w-1/3 my-auto">
+            <Image
+              src={event.image?.[0] || ''}
+              alt="Event Preview"
+              width={300}
+              height={200}
+              className="rounded"
+            />
+          </div>
+          <div className="pl-2">
+            <p className="font-bold">{event.name}</p>
+            <p className="font-gray-800">{event.location.name}</p>
+            <p className="text-gray-600">{timeAndDate(event.startDate)}</p>
+          </div>
+        </div>
+
         <div className="text-sm mt-2 whitespace-pre-line">
           {event.description}
         </div>

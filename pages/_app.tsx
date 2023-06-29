663fd7915cf2ca30634cd1b908115cd2acb920dc
Levin Keller <post@levinkeller.de>
2023-06-29 17:35:30 +0200

    feat: fuege ein Impressum hinzu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import { NavBar } from 'components/NavBar'
+import { Footer } from 'components/Footer'
 import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col justify-between">
       <DefaultSeo title="Rössing" description="Alle Infos über Rössing" />
       <NavBar />
-      <Component {...pageProps} />
-    </>
+      <div className="flex-grow">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
   )
 }
 

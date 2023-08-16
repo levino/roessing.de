0f580a99752695d609c164dffabb7cdecf69b1a8
Levin Keller <post@levinkeller.de>
2023-08-16 10:52:45 +0200

    feat: run lighthouse ci in ci

---
+++ lighthouserc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    collect: {
+      url: ['http://localhost:3000', 'http://localhost:3000/events'],
+      startServerCommand: 'yarn start',
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'bf-cache': 'off',
+        'csp-xss': 'off',
+        'image-aspect-ratio': 'off',
+        'tap-targets': 'off',
+        'heading-order': 'off',
+        'uses-responsive-images': 'off',
+      },
+    },
+  },
+}

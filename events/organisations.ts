2dbf12e2401c8bd1307339b02d705df179783510
Levin Keller <post@levinkeller.de>
2023-09-26 19:07:13 +0200

    chore: refactor app feat: add cdu events in october

---
+++ events/organisations.ts
@@ -0,0 +1,54 @@
+import { Organization as DTSOrganization } from 'schema-dts'
+type Organization = Exclude<DTSOrganization, string>
+export const organisations = {
+  vsv: {
+    name: 'Volkssportvereinigung von 1897 Rössing e.V.',
+    '@type': 'Organization',
+    address: {
+      '@type': 'PostalAddress',
+      postalCode: '31171',
+      streetAddress: 'Pfarrstraße 6',
+      addressLocality: 'Nordstemmen',
+    },
+    telephone: '+4950697415',
+  } as Organization,
+  kleingartenVerein: {
+    '@type': 'Organization',
+    name: 'Kleingartenverein Rössing e.V.',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Am Rössingbach',
+      addressLocality: 'Nordstemmen',
+      postalCode: '31171',
+      telephone: '+4950693109',
+    },
+  } as Organization,
+  dorfpflege: {
+    '@type': 'Organization',
+    name: 'Dorfpflege Rössing e.V.',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Stettiner Straße 3',
+      addressLocality: 'Nordstemmen',
+      postalCode: '31171',
+    },
+  } as Organization,
+  kulturkreis: {
+    '@type': 'Organization',
+    name: 'Kulturkreis Rössing e.V.',
+    email: 'roessingerkulturkreis@gmail.com',
+    telephone: '+49506934413',
+  } as Organization,
+  cdu: {
+    '@type': 'Organization',
+    name: 'CDU Rössing',
+    email: 'udietrich-roessing@t-online.de',
+    telephone: '+49506934413',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Maschstraße 6',
+      addressLocality: 'Nordstemmen',
+      postalCode: '31171',
+    },
+  } as Organization,
+}

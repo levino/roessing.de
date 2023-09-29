6ad3201be96895c30f6228326d972fa63b04ce84
Levin Keller <post@levinkeller.de>
2023-09-29 13:12:51 +0200

    fix: stelle og daten fuer events wieder her

--- app/events/[...slug]/page.tsx
+++ app/events/[...slug]/page.tsx
@@ -1,14 +1,59 @@
 import { EventPage } from 'components/Events/EventPage'
-
+import { Event as EventType } from '@/data/events/event'
 import { getAllSlugs, getEvent } from '../../../data/events'
 import * as A from 'fp-ts/Array'
 import * as Str from 'fp-ts/string'
 import { PathReporter } from 'io-ts/PathReporter'
 import { isLeft } from 'fp-ts/Either'
+import { flow } from 'fp-ts/function'
+import * as S from 'fp-ts-std/Struct'
+import { StaticImageData } from 'next/image'
+import * as E from 'fp-ts/Either'
+import * as O from 'fp-ts/Option'
 interface EventPageProps {
   params: { slug: string[] }
 }
+const getTitle = (event: EventType) => `${event.data.name} - rössing.de`
 
+const getDescriptionObject: (event: EventType) => { description?: string } =
+  flow(
+    S.get('data'),
+    S.get('description'),
+    O.match(
+      () => ({}),
+      (description) => ({ description })
+    )
+  )
+
+const getImagesObject: (event: EventType) => { src?: string } = flow(
+  S.get('data'),
+  S.get('image'),
+  O.match(
+    () => ({}),
+    ({ src }: StaticImageData) => ({ images: [src] })
+  )
+)
+
+export const generateMetadata = async ({
+  params: { slug },
+}: EventPageProps) => {
+  const postValidation = getEvent({
+    file: await import(`events/${slug.join('-')}.mdx`),
+    slug: slug.join('/'),
+  })
+  return E.match(
+    () => ({}),
+    (event: EventType) => ({
+      title: getTitle(event),
+      ...getDescriptionObject(event),
+      openGraph: {
+        title: getTitle(event),
+        ...getDescriptionObject(event),
+        ...getImagesObject(event),
+      },
+    })
+  )(postValidation)
+}
 export const generateStaticParams = () =>
   getAllSlugs()
     .then(A.map(Str.split('/')))

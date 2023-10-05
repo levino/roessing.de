fcc2f63fd7df1109bb73dddbae07232d7557c50b
Levin Keller <post@levinkeller.de>
2023-10-05 10:50:09 +0200

    feat: füge events hinzu

--- data/events/events.test.ts
+++ data/events/events.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test } from 'bun:test'
 import { EventData } from './event'
 import { pipe } from 'fp-ts/function'
 import * as E from 'fp-ts/Either'

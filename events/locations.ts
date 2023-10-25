32c92a17564204589f5dd90d5de0f25e59327e43
Levin Keller <post@levinkeller.de>
2023-10-25 12:12:27 +0200

    fix: readd removed locations

--- events/locations.ts
+++ events/locations.ts
@@ -24,6 +24,43 @@ export const locations = {
       ...nordstemmen,
     },
   } as Place,
+  hofKoenneke: {
+    '@type': 'Place',
+    name: 'Hof Könneke',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Pfarrstr. 6',
+      ...nordstemmen,
+    },
+  } as Place,
+  kleingartenVerein: {
+    '@type': 'Place',
+    name: 'Kleingartenverein Rössing e.V.',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Am Rössingbach',
+      ...nordstemmen,
+      telephone: '+4950693109',
+    },
+  } as Place,
+  kirche: {
+    '@type': 'Place',
+    name: 'St. Peter und Paul Kirche',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Kirchstraße 9',
+      ...nordstemmen,
+    },
+  } as Place,
+  dorfgemeinschaftshausBarnten: {
+    '@type': 'Place',
+    name: 'Dorfgemeinschaftshaus Barnten',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Glückaufstraße 1',
+      ...nordstemmen,
+    },
+  } as Place,
   langeWanne: {
     '@type': 'Place',
     name: 'Lange Wanne',

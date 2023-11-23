963d6090ffe31011a28af6ad0fc09d1290ba182a
Levin Keller <post@levinkeller.de>
2023-11-23 10:28:55 +0000

    fix: styles of navbar in dark mode and on mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-
 import sitemap from '@astrojs/sitemap'
+
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xn--rssing-wxa.de',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), react()],
   vite: {
     ssr: {
-      noExternal: ['fp-ts'],
+      noExternal: ['fp-ts', 'usehooks-ts'],
     },
   },
 })
